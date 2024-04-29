517fede12b9ee39fa2a822a7d9facb4feb888c05
Yifei Li <923120244a@gmail.com>
2024-04-29 14:15:13 -0400

    first commit

---
+++ MyMusic.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMusic.Api.Extensions
+{
+    public class SwaggerExtensions
+    {
+        
+    }
+}
---
+++ MyMusic.Core/Models/Auth/Role.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MyMusic.Core.Models
+{
+    public class Role : IdentityRole<Guid>
+    {
+        
+    }
+}
